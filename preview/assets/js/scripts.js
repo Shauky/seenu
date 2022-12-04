569467ba6bbbe6159d828af9512aa7255667fd80
deviant <devi4nt@Shaukaths-MacBook-Pro.local>
2022-12-04 08:18:04 +0500

    Done with base styles with tailwind

--- preview/assets/js/scripts.js
+++ preview/assets/js/scripts.js
@@ -1,1 +1,13 @@
 // This is your main script file
+$(document).ready(function(){
+
+    $('div.ui-message').addClass('highlight');
+
+    $('p:contains(ި),h3:contains("ި")').addClass('rightlines');
+
+    $('.ui-close').click(function(){
+        $(this).parents('.highlight').hide();
+    })
+
+
+});
